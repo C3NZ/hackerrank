05aa601ddf72acb386ee04d413255d255eb8a315
C3NZ <cenz@cenz.io>
2018-10-26 09:30:19 -0700

    Completed the Calendar module problem

---
+++ python/getday.py
@@ -0,0 +1,10 @@
+'''
+    Using the calendar module to get a specific date
+'''
+import calendar
+
+if __name__ == '__main__':
+    user_input = list(map(int, input("Enter in a date in MM DD YYYY format: ").split()))
+    month, day, year = user_input[0], user_input[1], user_input[2]
+    the_day = calendar.weekday(year, month, day)
+    print(calendar.day_name[the_day].upper())
---
+++ python/rangoli.py
@@ -0,0 +1,17 @@
+'''
+Alphabet Rangoli
+
+Problem description can be found here: https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+'''
+
+import string
+
+def print_rangoli(size):
+    max_width = ((size * 2) - 1) - 1
+    max_height = ((size * 2) - 1)
+
+    for i in max_height:
+        for i in max_width:
+
+
+
